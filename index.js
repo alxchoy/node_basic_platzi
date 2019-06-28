fb8ddc76d8484bf71ba383c6599bc1e324f29257
Alexander Choy <alexanderchoy18@gmail.com>
2019-06-28 00:07:11 -0500

    use require en modulos externos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const cowsay = require('cowsay');
+
+console.log(cowsay.say({
+  text: 'Holaa, soy cowsay',
+  e: 'o0',
+  T: 'U'
+}));
