4e1738d637cc5f1f20d2ea0919fb65fbcceb3154
Alexander Choy <alexanderchoy18@gmail.com>
2019-06-27 23:54:47 -0500

    uso de export y require  para modulos locales

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-/**
- * *************
- * Comience aqui
- * *************
- */
+const math = require('./math');
+const greet = require('./greetings'); // trae el index.js
+const hello = require('./greetings/hello');
+
+console.log(math.add(4, 5));
+console.log(math.divide(4, 5));
+console.log(math.multiply(4, 5));
+console.log(math.substract(4, 5));
+
+console.log(greet.greet('Choy'));
+
+console.log(hello.sayHello('Alex'));
